d0b64cde08ef8b5837aedcdefc318c9948416600
Ian T Small <ian.small@thekeepstudios.com>
2017-04-07 00:20:37 +0000

    Modified fix_permissions.sh

--- fix_permissions.sh
+++ fix_permissions.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-chown www-data:www-data -R . ;\
-find . -type d -exec chmod 755 {} ;\
-find . -type f -exec chmod 644 {} ;
+chown www-data:www-data -R . && \
+find . -type d -exec chmod 755 {} + && \
+find . -type f -exec chmod 644 {} +
 
